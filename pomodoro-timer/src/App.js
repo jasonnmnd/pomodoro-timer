307aaa8770f642cd2085361bb41329f3d02ee82f
Jason Dong <85291489+jasonnmnd@users.noreply.github.com>
2022-06-28 19:04:40 -0400

    finishing timer

--- pomodoro-timer/src/App.js
+++ pomodoro-timer/src/App.js
@@ -3,18 +3,29 @@ import './App.css';
 import Timer from './Timer';
 import Settings from './Settings';
 import {useState} from 'react';
+import SettingsContext from './SettingsContext';
 
 function App() {
 
   const [showSettings, setShowSettings] = useState(false)
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
 
   return (
     <main>
+      <SettingsContext.Provider value={{
+        workMinutes: workMinutes,
+        breakMinutes: breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+        setShowSettings
+      }}>
       {showSettings ?
       <Settings />
       :
       <Timer />
       }
+      </SettingsContext.Provider>
     </main>
   );
 }
--- pomodoro-timer/src/Settings.js
+++ pomodoro-timer/src/Settings.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ReactSlider from 'react-slider'
+import BackButton from './BackButton';
+import SettingsContext from './SettingsContext';
 import './slider.css';
 
 function Settings() {
+    const settingsInfo = useContext(SettingsContext);
   return (
     <div style={{textAlign: 'left'}}>
-        <label>Work Minutes: </label>
+        <label>Work Minutes: {settingsInfo.workMinutes}:00</label>
 
         <ReactSlider
             className={'slider'}
             thumbClassName={'thumb'}
             trackClassName={'track'}
-            value={45}
+            value={settingsInfo.workMinutes}
+            onChange={newValue => settingsInfo.setWorkMinutes(newValue)}
             min={1}
             max={120}
         ></ReactSlider>
 
-        <label>Break Minutes: </label>
+        <label>Break Minutes: {settingsInfo.breakMinutes}:00</label>
+
+        <ReactSlider
+            className={'slider green'}
+            thumbClassName={'thumb'}
+            trackClassName={'track'}
+            value={settingsInfo.breakMinutes}
+            onChange={newValue => settingsInfo.setBreakMinutes(newValue)}
+            min={1}
+            max={120}
+        ></ReactSlider>
+        
+        <div style={{textAlign:'center', marginTop:'20px'}}>
+            <BackButton onClick={() => settingsInfo.setShowSettings(false)}/>
+        </div>
+        
     </div>
   )
 }
